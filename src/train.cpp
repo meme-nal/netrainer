85d21f3b6611ca2a19a635eb4aa2a99a562060b4
me_me <ichshlanger2@gmail.com>
2025-04-13 01:01:21 +0300

    new: measuring quality on test batches added

--- src/train.cpp
+++ src/train.cpp
@@ -12,6 +12,8 @@
 #include <iostream>
 #include <vector>
 #include <memory>
+#include <algorithm>
+#include <random>
 
 
 int main(int argc, char** argv) {
@@ -33,9 +35,9 @@ int main(int argc, char** argv) {
   
   CommonOptions common = loadCommonOptions(path_to_nn_cfg);
 
+  // start from epoch if setted
   std::shared_ptr<NN> model = loadModel(path_to_nn_cfg);
   model->to(common._device);
-  model->train();
 
   std::shared_ptr<torch::optim::Optimizer> optimizer = loadOptimizer(path_to_nn_cfg, model);
   std::shared_ptr<BaseLayer> criterion = loadCriterion(path_to_nn_cfg);
@@ -43,16 +45,37 @@ int main(int argc, char** argv) {
 
   std::cout << "Count of parameters: " << count_model_params(model) << "\n\n";
 
+  // split on train and test batches, add metric
   std::vector<Batch> batches = loadBatches(path_to_batches);
-  auto trainDataset = TextDataset(batches).map(torch::data::transforms::Stack<>());
 
-  auto dataLoader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(std::move(trainDataset), common._mbatch_size);
+  size_t train_part = static_cast<int>((common._train_part / 100.f) * batches.size());
+  size_t test_part = batches.size() - train_part;
+
+  auto rng = std::default_random_engine {};
+  std::shuffle(std::begin(batches), std::end(batches), rng);
+
+  std::vector<Batch> train_batches;
+  for (size_t i {0}; i < train_part; ++i) {
+    train_batches.push_back(batches[i]);
+  }
   
+  std::vector<Batch> test_batches;
+  for (size_t i {train_part}; i < train_part + test_part; ++i) {
+    test_batches.push_back(batches[i]);
+  }
+
+  auto trainDataset = TextDataset(train_batches).map(torch::data::transforms::Stack<>());
+  auto testDataset = TextDataset(test_batches).map(torch::data::transforms::Stack<>());
+
+  auto trainDataLoader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(std::move(trainDataset), common._mbatch_size);
+  auto testDataLoader = torch::data::make_data_loader<torch::data::samplers::RandomSampler>(std::move(testDataset), common._mbatch_size);
+
   for (size_t epoch {0}; epoch < common._num_epochs; ++epoch) {
-    size_t bi {0};
-    float mean_loss {0};
+    model->train();
+    size_t bi_train {0};
+    float mean_train_loss {0};
     std::cout << "=== Epoch: " << epoch << " ===\n";
-    for (auto& batch : *dataLoader) {
+    for (auto& batch : *trainDataLoader) {
       optimizer->zero_grad();
 
       torch::Tensor features = batch.data.to(common._device);
@@ -65,14 +88,38 @@ int main(int argc, char** argv) {
       torch::Tensor prediction = model->forward(features).to(common._device);
       torch::Tensor loss = criterion->forward(prediction, labels);
       
-      mean_loss += loss.item<float>();
+      mean_train_loss += loss.item<float>();
       loss.backward();
       optimizer->step();
 
-      std::cout << "loss | " << bi << " | : " << loss.item<float>() << '\n';
-      ++bi;
+      std::cout << "loss | " << bi_train << " | : " << loss.item<float>() << '\n';
+      ++bi_train;
     }
-    std::cout << "=== Mean Loss: " << mean_loss / (bi + 1) << " ===\n\n";
+    std::cout << "=== Mean Train Loss: " << mean_train_loss / (bi_train + 1) << " ===\n\n";
+    
+    size_t bi_test {0};
+    float mean_test_loss {0};
+    std::cout << "=== Epoch: " << epoch << " ===\n";
+    for (auto& batch : *testDataLoader) {
+      model->eval();
+      torch::Tensor features = batch.data.to(common._device);
+      torch::Tensor labels   = batch.target.squeeze().to(common._device);
+      
+      if (label_generator) {
+        labels = (*label_generator)(labels);
+      }
+
+      torch::Tensor prediction = model->forward(features).to(common._device);
+      torch::Tensor loss = criterion->forward(prediction, labels);
+      
+      mean_test_loss += loss.item<float>();
+      ++bi_test;
+    }
+    std::cout << "=== Mean Test Loss: " << mean_test_loss / (bi_test + 1) << " ===\n\n";
+
+     std::string to_terminal = std::to_string(epoch) + ": " + std::to_string(mean_test_loss / (bi_test + 1));
+     print_to_terminal(to_terminal);
+
     std::string path_to_model_states = common._to_save_path;
     std::filesystem::path model_state_dir = path_to_model_states + "/state_" + std::to_string(epoch);
     std::filesystem::create_directory(model_state_dir);
